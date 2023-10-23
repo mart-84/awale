98e477e7fcde72e9940939fb4cb428573a809444
galet.arthur <galet.arthur@gmail.com>
2023-10-23 15:46:50 +0200

    premiere gestion des commandes

--- server/server.c
+++ server/server.c
@@ -11,7 +11,7 @@ static void init(void)
 #ifdef WIN32
    WSADATA wsa;
    int err = WSAStartup(MAKEWORD(2, 2), &wsa);
-   if(err < 0)
+   if (err < 0)
    {
       puts("WSAStartup failed !");
       exit(EXIT_FAILURE);
@@ -38,7 +38,7 @@ static void app(void)
 
    fd_set rdfs;
 
-   while(1)
+   while (1)
    {
       int i = 0;
       FD_ZERO(&rdfs);
@@ -50,37 +50,37 @@ static void app(void)
       FD_SET(sock, &rdfs);
 
       /* add socket of each client */
-      for(i = 0; i < actual; i++)
+      for (i = 0; i < actual; i++)
       {
          FD_SET(clients[i].sock, &rdfs);
       }
 
-      if(select(max + 1, &rdfs, NULL, NULL, NULL) == -1)
+      if (select(max + 1, &rdfs, NULL, NULL, NULL) == -1)
       {
          perror("select()");
          exit(errno);
       }
 
       /* something from standard input : i.e keyboard */
-      if(FD_ISSET(STDIN_FILENO, &rdfs))
+      if (FD_ISSET(STDIN_FILENO, &rdfs))
       {
          /* stop process when type on keyboard */
          break;
       }
-      else if(FD_ISSET(sock, &rdfs))
+      else if (FD_ISSET(sock, &rdfs))
       {
          /* new client */
-         SOCKADDR_IN csin = { 0 };
+         SOCKADDR_IN csin = {0};
          size_t sinsize = sizeof csin;
          int csock = accept(sock, (SOCKADDR *)&csin, &sinsize);
-         if(csock == SOCKET_ERROR)
+         if (csock == SOCKET_ERROR)
          {
             perror("accept()");
             continue;
          }
 
          /* after connecting the client sends its name */
-         if(read_client(csock, buffer) == -1)
+         if (read_client(csock, buffer) == -1)
          {
             /* disconnected */
             continue;
@@ -88,15 +88,18 @@ static void app(void)
 
          // TODO : vérifier que le nom n'est pas déjà pris
          int pseudoPris = 0;
-         for (int i = 0; i <= actual; i++) {
-            if (strcmp(clients[i].name, buffer) == 0) {
+         for (int i = 0; i <= actual; i++)
+         {
+            if (strcmp(clients[i].name, buffer) == 0)
+            {
                printf("Client name %s already taken\n", buffer);
                pseudoPris = 1;
                break;
             }
          }
 
-         if (pseudoPris) {
+         if (pseudoPris)
+         {
             write_client(csock, "Ce pseudo est déjà utilisé.\n");
             end_connection(csock);
             continue;
@@ -107,7 +110,7 @@ static void app(void)
 
          FD_SET(csock, &rdfs);
 
-         Client c = { csock };
+         Client c = {csock};
          strncpy(c.name, buffer, BUF_SIZE - 1);
          clients[actual] = c;
          actual++;
@@ -116,21 +119,48 @@ static void app(void)
       else
       {
          int i = 0;
-         for(i = 0; i < actual; i++)
+         for (i = 0; i < actual; i++)
          {
             /* a client is talking */
-            if(FD_ISSET(clients[i].sock, &rdfs))
+            if (FD_ISSET(clients[i].sock, &rdfs))
             {
                Client client = clients[i];
                int c = read_client(clients[i].sock, buffer);
                /* client disconnected */
-               if(c == 0)
+               if (c == 0)
                {
                   closesocket(clients[i].sock);
                   remove_client(clients, i, &actual);
                   strncpy(buffer, client.name, BUF_SIZE - 1);
                   strncat(buffer, " disconnected !", BUF_SIZE - strlen(buffer) - 1);
                   send_message_to_all_clients(clients, client, actual, buffer, 1);
+               }
+               else if (buffer[0] == '/')
+               {
+                  char* commande = strtok(buffer, " ");
+                  if (strcmp(commande, "/help") == 0) {
+                     printf("client %d (%s) : /help\n", i, clients[i].name);
+                     write_client(clients[i].sock, "Liste des commandes disponibles :\n");
+                     write_client(clients[i].sock, " - /help : affiche ce message d'aide\n");
+                     write_client(clients[i].sock, " - /listejoueurs : affiche les autres joueurs connectes\n");
+      
+                  } else if (strcmp(commande, "/listejoueurs") == 0) {
+                     printf("client %d (%s) : /listejoueurs\n", i, clients[i].name);
+                     if (actual == 1) {
+                        write_client(clients[i].sock, "Aucun autre joueur n'est connecte\n");
+                     } else {
+                        write_client(clients[i].sock, "Liste des joueurs connectes : \n");
+                        for (int j = 0; j < actual; j++) {
+                           if (j != i) {
+                              write_client(clients[i].sock, " - ");
+                              write_client(clients[i].sock, clients[j].name);
+                              write_client(clients[i].sock, "\n");
+                           }
+                        }
+                     }
+                  } else if (strcmp(commande, "/duel") == 0) {
+                     printf("client %d (%s) : /duel\n", i, clients[i].name);
+                     
                }
                else
                {
@@ -149,7 +179,7 @@ static void app(void)
 static void clear_clients(Client *clients, int actual)
 {
    int i = 0;
-   for(i = 0; i < actual; i++)
+   for (i = 0; i < actual; i++)
    {
       closesocket(clients[i].sock);
    }
@@ -168,12 +198,12 @@ static void send_message_to_all_clients(Client *clients, Client sender, int actu
    int i = 0;
    char message[BUF_SIZE];
    message[0] = 0;
-   for(i = 0; i < actual; i++)
+   for (i = 0; i < actual; i++)
    {
       /* we don't send message to the sender */
-      if(sender.sock != clients[i].sock)
+      if (sender.sock != clients[i].sock)
       {
-         if(from_server == 0)
+         if (from_server == 0)
          {
             strncpy(message, sender.name, BUF_SIZE - 1);
             strncat(message, " : ", sizeof message - strlen(message) - 1);
@@ -187,9 +217,9 @@ static void send_message_to_all_clients(Client *clients, Client sender, int actu
 static int init_connection(void)
 {
    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-   SOCKADDR_IN sin = { 0 };
+   SOCKADDR_IN sin = {0};
 
-   if(sock == INVALID_SOCKET)
+   if (sock == INVALID_SOCKET)
    {
       perror("socket()");
       exit(errno);
@@ -199,13 +229,18 @@ static int init_connection(void)
    sin.sin_port = htons(PORT);
    sin.sin_family = AF_INET;
 
-   if(bind(sock,(SOCKADDR *) &sin, sizeof sin) == SOCKET_ERROR)
+   int reuse = 1;
+   if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(int)) == -1) {
+      perror("setsockopt");
+   }
+
+   if (bind(sock, (SOCKADDR *)&sin, sizeof sin) == SOCKET_ERROR)
    {
       perror("bind()");
       exit(errno);
    }
 
-   if(listen(sock, MAX_CLIENTS) == SOCKET_ERROR)
+   if (listen(sock, MAX_CLIENTS) == SOCKET_ERROR)
    {
       perror("listen()");
       exit(errno);
@@ -223,7 +258,7 @@ static int read_client(SOCKET sock, char *buffer)
 {
    int n = 0;
 
-   if((n = recv(sock, buffer, BUF_SIZE - 1, 0)) < 0)
+   if ((n = recv(sock, buffer, BUF_SIZE - 1, 0)) < 0)
    {
       perror("recv()");
       /* if recv error we disonnect the client */
@@ -237,7 +272,7 @@ static int read_client(SOCKET sock, char *buffer)
 
 static void write_client(SOCKET sock, const char *buffer)
 {
-   if(send(sock, buffer, strlen(buffer), 0) < 0)
+   if (send(sock, buffer, strlen(buffer), 0) < 0)
    {
       perror("send()");
       exit(errno);
