b66a37c17c2abed890572c5592b6f56ec5fdf8b9
galet.arthur <galet.arthur@gmail.com>
2023-10-27 09:38:23 +0200

    ajout de la tokénisation de string (sert à rien)

---
+++ server/util.h
@@ -0,0 +1,10 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int tokeniserChaineDeCaracteres(char* buffer, char delimiter, char*** tokens);
+
+#endif /* guard*/
